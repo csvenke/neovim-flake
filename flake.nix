3ff801487bff2b65b0fc05116bade66db62decb6
csvenke <csvenke@users.noreply.github.com>
2024-09-14 19:21:35 +0200

    refactor: update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
       ];
-      perSystem = { pkgs, system, ... }:
+      perSystem = { system, ... }:
         let
           pkgs = import nixpkgs {
             inherit system;

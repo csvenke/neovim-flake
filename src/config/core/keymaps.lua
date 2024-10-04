825648dd4c9a22af5057ada91ba2a6cf12f5bb07
csvenke <csvenke@users.noreply.github.com>
2024-10-04 22:03:42 +0200

    feat(core): update keymaps

--- src/config/core/keymaps.lua
+++ src/config/core/keymaps.lua
@@ -16,3 +16,6 @@ vim.keymap.set("i", "<C-l>", "<Esc>la", { desc = "Shift right" })
 vim.keymap.set("i", "<C-h>", "<Esc>i", { desc = "Shift left" })
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.api.nvim_create_user_command("W", "w", {})
+vim.api.nvim_create_user_command("Wa", "wa", {})

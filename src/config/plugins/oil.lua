065c395ae1227e886507db6f676019830c2d0933
csvenke <csvenke@users.noreply.github.com>
2024-10-13 16:48:41 +0200

    feat(oil): update oil config

--- src/config/plugins/oil.lua
+++ src/config/plugins/oil.lua
@@ -1,16 +1,31 @@
 require("oil").setup({
   default_file_explorer = true,
   use_default_keymaps = false,
+  skip_confirm_for_simple_edits = true,
+  watch_for_changes = true,
   keymaps = {
     ["_"] = "actions.open_cwd",
     ["-"] = "actions.parent",
-    ["."] = "actions.cd",
     ["<CR>"] = "actions.select",
-    ["g."] = "actions.toggle_hidden",
+    ["g."] = "actions.cd",
     ["gx"] = "actions.open_external",
+    ["ff"] = {
+      function()
+        require("telescope.builtin").find_files({
+          cwd = require("oil").get_current_dir(),
+        })
+      end,
+      mode = "n",
+      nowait = true,
+      desc = "Find files in current directory",
+    },
   },
   view_options = {
     show_hidden = true,
+    is_always_hidden = function(name)
+      local hidden_names = { "..", ".git" }
+      return vim.tbl_contains(hidden_names, name)
+    end,
   },
 })
 

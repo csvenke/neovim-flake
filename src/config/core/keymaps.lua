465f42456819c45c61b980c117a3ad52df92e088
csvenke <csvenke@users.noreply.github.com>
2024-10-13 16:43:45 +0200

    feat(core): remove unused keymaps

--- src/config/core/keymaps.lua
+++ src/config/core/keymaps.lua
@@ -3,9 +3,6 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>qq", "<cmd>wqa<cr>", { desc = "[q]uit" })
 vim.keymap.set("n", "<leader>qQ", "<cmd>qa<cr>", { desc = "[q]uit (without saving)" })
 
-vim.keymap.set("n", "<leader>-", "<cmd>split<cr>", { desc = "split horisontal" })
-vim.keymap.set("n", "<leader>|", "<cmd>vsplit<cr>", { desc = "split vertical" })
-
 vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<cr>", { desc = "save" })
 vim.keymap.set("n", "<C-q>", "<cmd>q<cr>", { desc = "close" })
 

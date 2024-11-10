5ecde545085622ba7e97d186da35e461fbac4440
csvenke <csvenke@users.noreply.github.com>
2024-11-10 16:19:21 +0100

    feat(cmp): remove unused mappings

--- src/config/plugins/cmp.lua
+++ src/config/plugins/cmp.lua
@@ -26,8 +26,6 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<Enter>"] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete({}),

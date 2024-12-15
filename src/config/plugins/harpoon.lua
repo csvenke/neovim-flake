ab4e1335d2b75217289d362ceb50bc02d7ef3c87
csvenke <csvenke@users.noreply.github.com>
2024-12-15 15:55:18 +0100

    feat(harpoon): update harpoon keymaps

--- src/config/plugins/harpoon.lua
+++ src/config/plugins/harpoon.lua
@@ -2,12 +2,12 @@ local harpoon = require("harpoon")
 
 harpoon.setup()
 
-vim.keymap.set("n", "<C-a>", function()
+vim.keymap.set("n", "<M-a>", function()
   harpoon:list():add()
   vim.notify("File added")
 end, { desc = "harpoon file" })
 
-vim.keymap.set("n", "<C-e>", function()
+vim.keymap.set("n", "<M-e>", function()
   local toggle_opts = {
     border = "rounded",
     title_pos = "center",
@@ -17,26 +17,18 @@ vim.keymap.set("n", "<C-e>", function()
   harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts)
 end, { desc = "open harpoon window" })
 
-vim.keymap.set("n", "<C-p>", function()
-  harpoon:list():prev()
-end, { desc = "goto [p]revious" })
-
-vim.keymap.set("n", "<C-n>", function()
-  harpoon:list():next()
-end, { desc = "goto [n]ext" })
-
-vim.keymap.set("n", "<leader>1", function()
+vim.keymap.set("n", "<M-1>", function()
   harpoon:list():select(1)
 end, { desc = "goto [1]" })
 
-vim.keymap.set("n", "<leader>2", function()
+vim.keymap.set("n", "<M-2>", function()
   harpoon:list():select(2)
 end, { desc = "goto [2]" })
 
-vim.keymap.set("n", "<leader>3", function()
+vim.keymap.set("n", "<M-3>", function()
   harpoon:list():select(3)
 end, { desc = "goto [3]" })
 
-vim.keymap.set("n", "<leader>4", function()
+vim.keymap.set("n", "<M-4>", function()
   harpoon:list():select(4)
 end, { desc = "goto [4]" })

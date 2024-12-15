0e017e755aa1ed9fc227e2dd27d8f293ee64bdcd
csvenke <csvenke@users.noreply.github.com>
2024-12-15 15:56:11 +0100

    feat(keymaps): add quick fix list navigation and improve keymap organization

--- src/config/core/keymaps.lua
+++ src/config/core/keymaps.lua
@@ -1,17 +1,21 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
 
+-- quitting
 vim.keymap.set("n", "<leader>qq", "<cmd>wqa<cr>", { desc = "[q]uit" })
 vim.keymap.set("n", "<leader>qQ", "<cmd>qa<cr>", { desc = "[q]uit (without saving)" })
+vim.keymap.set("n", "<C-q>", "<cmd>q<cr>", { desc = "[q]uit" })
 
-vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<cr>", { desc = "save" })
-vim.keymap.set("n", "<C-q>", "<cmd>q<cr>", { desc = "close" })
+-- saving
+vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<cr>", { desc = "[s]ave" })
+vim.api.nvim_create_user_command("W", "w", {})
+vim.api.nvim_create_user_command("Wa", "wa", {})
+vim.api.nvim_create_user_command("WA", "wa", {})
 
 vim.keymap.set("i", "<C-j>", "{}<Left>", { desc = "{" })
 vim.keymap.set("i", "<C-k>", "[]<Left>", { desc = "[" })
 vim.keymap.set("i", "<C-l>", "<Esc>la", { desc = "Shift right" })
 vim.keymap.set("i", "<C-h>", "<Esc>i", { desc = "Shift left" })
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
-vim.api.nvim_create_user_command("W", "w", {})
-vim.api.nvim_create_user_command("Wa", "wa", {})
+-- quick fix list
+vim.keymap.set("n", "<C-n>", "<cmd>cnext<cr>", { desc = "qf [n]ext" })
+vim.keymap.set("n", "<C-p>", "<cmd>cprev<cr>", { desc = "qf [p]rev" })

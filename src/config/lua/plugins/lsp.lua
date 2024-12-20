3447e303df9ec5ba9d02e3feabd2fc6020dd831f
csvenke <csvenke@users.noreply.github.com>
2024-12-20 23:40:21 +0100

    feat(lsp): update blink.cmp configuration

--- src/config/lua/plugins/lsp.lua
+++ src/config/lua/plugins/lsp.lua
@@ -240,7 +240,15 @@ require("fidget").setup({})
 require("neodev").setup({})
 require("neoconf").setup({})
 require("blink.cmp").setup({
-  signature = { enabled = true },
+  keymap = { preset = "default" },
+  appearance = {
+    use_nvim_cmp_as_default = true,
+    nerd_font_variant = "mono",
+  },
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer" },
+  },
+  signature = { enabled = false },
 })
 
 local lspconfig = require("lspconfig")

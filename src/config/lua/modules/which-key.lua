89adce3436951f77c6595acbbb14c212e7c84466
csvenke <csvenke@users.noreply.github.com>
2025-01-03 15:43:43 +0100

    feat(which-key): change labels for icons

--- src/config/lua/modules/which-key.lua
+++ src/config/lua/modules/which-key.lua
@@ -3,17 +3,18 @@ local wk = require("which-key")
 wk.add({
   {
     mode = { "n", "v" },
-    { "<leader>a", group = "[a]i" },
+    { "<leader>a", group = "ai" },
   },
-  { "<leader>b", group = "[b]uffer" },
+  { "<leader>b", group = "buffer" },
   {
-    mode = { "n", "x" },
-    { "<leader>c", group = "[c]ode" },
+    mode = { "n", "v" },
+    { "<leader>c", group = "code" },
   },
-  { "<leader>d", group = "[d]ebug" },
-  { "<leader>g", group = "[g]it" },
-  { "<leader>q", group = "[q]uit" },
-  { "<leader>s", group = "[s]earch" },
-  { "<leader>t", group = "[t]est" },
+  { "<leader>d", group = "debug" },
+  { "<leader>g", group = "git" },
+  { "<leader>q", group = "quit" },
+  { "<leader>s", group = "search" },
+  { "<leader>t", group = "test" },
+  { "<leader>w", group = "git worktree" },
   { "<leader>l", group = "logs" },
 })

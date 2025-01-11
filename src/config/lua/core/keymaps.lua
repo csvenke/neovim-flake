bf8229da1fdeaaa0fcf9579b4c238e42e1022b92
csvenke <csvenke@users.noreply.github.com>
2025-01-11 15:59:27 +0100

    feat(keymaps): reorganize and update keymaps
    * add dedicated section for window navigation with CTRL+HJKL
    * modify insert mode movement to use arrow keys with CTRL
    * move macro disabling to top of file
    * update bracket insertion shortcuts to CTRL+I/O
    * group related keymaps with descriptive comments

--- src/config/lua/core/keymaps.lua
+++ src/config/lua/core/keymaps.lua
@@ -1,5 +1,9 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
 
+-- disable macros
+vim.keymap.set("n", "q", "<Nop>")
+vim.keymap.set("n", "Q", "<Nop>")
+
 -- quitting
 vim.keymap.set("n", "<leader>qq", "<cmd>wqa<cr>", { desc = "[q]uit" })
 vim.keymap.set("n", "<leader>qQ", "<cmd>qa<cr>", { desc = "[q]uit (without saving)" })
@@ -11,15 +15,22 @@ vim.api.nvim_create_user_command("W", "w", {})
 vim.api.nvim_create_user_command("Wa", "wa", {})
 vim.api.nvim_create_user_command("WA", "wa", {})
 
-vim.keymap.set("i", "<C-j>", "{}<Left>", { desc = "{" })
-vim.keymap.set("i", "<C-k>", "[]<Left>", { desc = "[" })
-vim.keymap.set("i", "<C-l>", "<Esc>la", { desc = "Shift right" })
-vim.keymap.set("i", "<C-h>", "<Esc>i", { desc = "Shift left" })
+-- navigation
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move left" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move right" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move down" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move up" })
 
--- quick fix list
+-- shifting
+vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Shift up" })
+vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Shift down" })
+vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Shift right" })
+vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Shift left" })
+
+-- programmer shortcuts
+vim.keymap.set("i", "<C-i>", "{}<Left>", { desc = "{" })
+vim.keymap.set("i", "<C-o>", "[]<Left>", { desc = "[" })
+
+-- quickfix list
 vim.keymap.set("n", "<C-n>", "<cmd>cnext<cr>", { desc = "qf [n]ext" })
 vim.keymap.set("n", "<C-p>", "<cmd>cprev<cr>", { desc = "qf [p]rev" })
-
--- remember, no macros
-vim.keymap.set("n", "q", "<Nop>")
-vim.keymap.set("n", "Q", "<Nop>")

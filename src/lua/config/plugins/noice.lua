fcb61d792ba80747a4785f81065bb8d8eed6abf4
csvenke <csvenke@users.noreply.github.com>
2025-01-15 17:30:38 +0100

    refactor(noice): remove unused noice.nvim preset configurations

--- src/lua/config/plugins/noice.lua
+++ src/lua/config/plugins/noice.lua
@@ -28,11 +28,8 @@ require("noice").setup({
     },
   },
   presets = {
-    bottom_search = false,
     command_palette = true,
     long_message_to_split = true,
-    inc_rename = true,
-    lsp_doc_border = false,
   },
 })
 

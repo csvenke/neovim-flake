3a9a25a7e7cc48187ec7d36ff096c505e1540a3e
csvenke <csvenke@users.noreply.github.com>
2025-01-18 20:32:25 +0100

    refactor(options): update options, disable swap files

--- src/lua/config/core/options.lua
+++ src/lua/config/core/options.lua
@@ -1,56 +1,64 @@
+-- Leader key configuration
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- General editor behavior
 vim.g.autoformat = false
-
 vim.opt.autowrite = true
-
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-
 vim.opt.mouse = "a"
-
 vim.opt.showmode = false
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
 
+-- Clipboard configuration
 vim.schedule(function()
   vim.opt.clipboard = "unnamedplus"
 end)
 
-vim.opt.breakindent = true
+-- Line numbers and UI display
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 10
+vim.opt.scrollbind = false
 
-vim.opt.undofile = true
+-- Search and completion settings
+vim.opt.hlsearch = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
+-- Indentation and formatting
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.smartcase = true
 vim.opt.smarttab = true
 vim.opt.smartindent = true
-vim.opt.ignorecase = true
+vim.opt.breakindent = true
+vim.cmd("set nowrap")
 
-vim.opt.signcolumn = "yes"
+-- Special characters display
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "  ",
+  trail = "·",
+  nbsp = "␣",
+  extends = "›",
+  precedes = "‹",
+}
 
-vim.opt.updatetime = 250
-
-vim.opt.timeoutlen = 300
-
+-- Split behavior
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
-vim.opt.list = true
-vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
+-- File handling
+vim.opt.undofile = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
 
-vim.opt.inccommand = "split"
-
-vim.opt.cursorline = true
-
-vim.opt.scrolloff = 10
-
-vim.opt.hlsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrollbind = false
-
-vim.cmd("set nowrap")
+-- Error bells
+vim.opt.errorbells = false
+vim.opt.visualbell = false

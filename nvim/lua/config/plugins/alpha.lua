7556d3cc3ee95827fe54d4782a4bff8ef644c986
csvenke <csvenke@users.noreply.github.com>
2025-01-27 23:48:20 +0100

    feat(alpha): display worktree name only

--- nvim/lua/config/plugins/alpha.lua
+++ nvim/lua/config/plugins/alpha.lua
@@ -4,12 +4,18 @@ local theta = require("alpha.themes.theta")
 local util = require("config.util")
 
 theta.file_icons.provider = "devicons"
-
 theta.buttons.val = {}
 
 local worktrees = util.get_worktrees()
 
 if not vim.tbl_isempty(worktrees) then
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = function()
+      vim.api.nvim_set_hl(0, "AlphaGitIcon", { fg = "#d08770" })
+    end,
+  })
+
   local shortcuts = { "q", "w", "e", "r", "a", "s", "d", "f", "z", "x", "c", "v" }
   local val = {}
 
@@ -23,28 +29,27 @@ if not vim.tbl_isempty(worktrees) then
   })
   table.insert(val, { type = "padding", val = 1 })
 
-  vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "*",
-    callback = function()
-      vim.api.nvim_set_hl(0, "AlphaGitIcon", { fg = "#d08770" })
-    end,
-  })
-
   for i, worktree in ipairs(worktrees) do
-    local path = worktree:match("^%S+")
-    local shortcut = shortcuts[i] or "-"
-    local icon = "󰊢"
-    local text = string.format("%s %s", icon, path)
-    local keybind = string.format("<cmd>cd %s | edit .<cr>", path)
-    local button = dashboard.button(shortcut, text, keybind)
+    local git_icon = "󰊢"
+    local worktree_path = worktree:match("^%S+")
+    local worktree_name = vim.fn.fnamemodify(worktree_path, ":t")
+    local worktree_shortcut = shortcuts[i] or "-"
+
+    local keybind = string.format("<cmd>cd %s | edit .<cr>", worktree_path)
+    local text = string.format("%s  %s", git_icon, worktree_name)
+    local button = dashboard.button(worktree_shortcut, text, keybind)
+
     button.opts.hl = {
-      { "AlphaGitIcon", 0, #icon },
+      { "AlphaGitIcon", 0, #git_icon },
     }
 
     table.insert(val, button)
   end
 
-  table.insert(theta.config.layout, 3, { type = "padding", val = 2 })
+  table.insert(theta.config.layout, 3, {
+    type = "padding",
+    val = 2,
+  })
   table.insert(theta.config.layout, 4, {
     type = "group",
     val = val,

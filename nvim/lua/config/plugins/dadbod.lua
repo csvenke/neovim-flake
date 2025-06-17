f96e45708a9562f240320289f34062bb3fa02f88
csvenke <csvenke@users.noreply.github.com>
2025-06-17 17:37:13 +0200

    feat(dadbod): enhance DBUI workflow with improved keybindings and autocmds
    * add helper functions for opening DBUI in new tab and executing queries
    * change DBUI position from right to left
    * expand keybinding options for DBUI toggle
    * add FileType autocmds for sql, dbui, and dbout buffers
    * implement F5 shortcut for query execution and Ctrl+Q for tab closing

--- nvim/lua/config/plugins/dadbod.lua
+++ nvim/lua/config/plugins/dadbod.lua
@@ -1,10 +1,40 @@
+local function dbui_tab()
+  vim.cmd("tabnew")
+  vim.cmd("DBUI")
+end
+
+local function execute_query()
+  vim.cmd("vertical % DB")
+  vim.cmd("wincmd =")
+end
+
 vim.g.db_ui_use_nerd_fonts = 1
-vim.g.db_ui_win_position = "right"
+vim.g.db_ui_win_position = "left"
 vim.g.db_ui_show_database_icon = 1
 vim.g.db_ui_use_nvim_notify = 1
 vim.g.db_ui_show_help = 0
 vim.g.db_ui_execute_on_save = 0
 
-vim.keymap.set("n", "<leader>DD", "<cmd>DBUIToggle<cr>", { desc = "[D]BUI [t]oggle" })
-vim.keymap.set("n", "<leader>Dt", "<cmd>DBUIToggle<cr>", { desc = "[D]BUI [t]oggle" })
+vim.keymap.set("n", "<leader>Dd", dbui_tab, { desc = "[D]BUI [t]oggle (shortcut)" })
+vim.keymap.set("n", "<leader>DD", dbui_tab, { desc = "[D]BUI [t]oggle (shortcut)" })
+vim.keymap.set("n", "<leader>Dt", dbui_tab, { desc = "[D]BUI [t]oggle" })
+vim.keymap.set("n", "<leader>DT", dbui_tab, { desc = "[D]BUI [t]oggle" })
 vim.keymap.set("n", "<leader>Da", "<cmd>DBUIAddConnection<cr>", { desc = "[D]BUI [a]dd connection" })
+
+local group = vim.api.nvim_create_augroup("user-dadbod-hooks", {})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "dbui", "sql", "dbout" },
+  group = group,
+  callback = function(event)
+    vim.keymap.set("n", "<C-q>", "<cmd>tabclose<cr>", { buffer = event.buf, desc = "[D]BUI close" })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "sql" },
+  group = group,
+  callback = function(event)
+    vim.keymap.set("n", "<F5>", execute_query, { buffer = event.buf, desc = "[D]BUI execute query" })
+  end,
+})

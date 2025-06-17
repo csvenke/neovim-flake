a7160ccc2a638285c651f252c060e8ad0ece62ca
csvenke <csvenke@users.noreply.github.com>
2025-06-17 14:55:10 +0200

    refactor(rest): simplify autocmd callback using event parameter

--- nvim/lua/config/plugins/rest.lua
+++ nvim/lua/config/plugins/rest.lua
@@ -2,9 +2,7 @@ vim.g.loaded_rest_nvim = true
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "http",
-  callback = function()
-    local buffer = vim.api.nvim_get_current_buf()
-
-    vim.keymap.set("n", "<F5>", "<cmd>Rest run<cr>", { buffer = buffer, desc = "Run request" })
+  callback = function(event)
+    vim.keymap.set("n", "<F5>", "<cmd>Rest run<cr>", { buffer = event.buf, desc = "Run request" })
   end,
 })

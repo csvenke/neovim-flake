da4b0fdc4a5dfd59398f9537b94144a74f4611a9
csvenke <csvenke@users.noreply.github.com>
2025-06-18 23:49:36 +0200

    refactor(rest): organize autocommands with dedicated group
    * create user-rest-hooks augroup for better organization
    * assign FileType autocmd to the new group

--- nvim/lua/config/plugins/rest.lua
+++ nvim/lua/config/plugins/rest.lua
@@ -1,7 +1,10 @@
 vim.g.loaded_rest_nvim = true
 
+local group = vim.api.nvim_create_augroup("user-rest-hooks", { clear = true })
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "http",
+  group = group,
   callback = function(event)
     vim.keymap.set("n", "<F5>", "<cmd>Rest run<cr>", { buffer = event.buf, desc = "Run request" })
   end,

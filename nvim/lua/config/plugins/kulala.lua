24f0f148ba43c0ac75e5269cd538213b303c4d90
csvenke <csvenke@users.noreply.github.com>
2025-06-19 17:43:17 +0200

    feat(kulala): update kulala configuration and UI controls
    * disable default keymaps to use custom bindings
    * configure UI to show body and headers panes by default
    * add tab navigation for kulala UI buffers
    * rename autocommand group to match kulala naming

--- nvim/lua/config/plugins/kulala.lua
+++ nvim/lua/config/plugins/kulala.lua
@@ -1,13 +1,29 @@
 local kulala = require("kulala")
+local kulala_ui = require("kulala.ui")
 
-kulala.setup({})
+kulala.setup({
+  kulala_keymaps = false,
+  global_keymaps = false,
+  ui = {
+    default_winbar_panes = { "body", "headers" },
+    disable_news_popup = true,
+  },
+})
 
-local group = vim.api.nvim_create_augroup("user-http-hooks", { clear = true })
+local group = vim.api.nvim_create_augroup("user-kulala-hooks", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "http",
   group = group,
   callback = function(event)
     vim.keymap.set("n", "<F5>", kulala.run, { buffer = event.buf, desc = "Run request" })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json.kulala_ui", "xml.kulala_ui" },
+  group = group,
+  callback = function(event)
+    vim.keymap.set("n", "<tab>", kulala_ui.toggle_headers, { buffer = event.buf, desc = "Show next" })
   end,
 })

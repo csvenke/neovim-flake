3787f583b118ece692d932f5804c4b0ee4a968f6
csvenke <csvenke@users.noreply.github.com>
2025-07-09 19:52:31 +0200

    feat(blink): update blink keymaps

--- nvim/lua/config/plugins/lsp/init.lua
+++ nvim/lua/config/plugins/lsp/init.lua
@@ -8,7 +8,10 @@ require("lazydev").setup({
   },
 })
 require("blink.cmp").setup({
-  keymap = { preset = "default" },
+  keymap = {
+    preset = "default",
+    ["<cr>"] = { "select_and_accept", "fallback" },
+  },
   appearance = {
     use_nvim_cmp_as_default = true,
     nerd_font_variant = "mono",

714d7fd2ae815370a902ae3d2b8f79ca0d7e12e1
csvenke <csvenke@users.noreply.github.com>
2025-11-22 13:08:07 +0100

    refactor(plugins): update smart-splits config
    * change <C-w>hjkl to use move_cursor functions instead of swap_buf
    * move swap_buf functions to <C-w>HJKL (uppercase) keybindings
    * update keymap descriptions for better clarity

--- nvim/lua/config/plugins/smart-splits.lua
+++ nvim/lua/config/plugins/smart-splits.lua
@@ -21,7 +21,14 @@ vim.keymap.set("n", "<M-l>", smartsplits.resize_right, { desc = "Resize right" }
 vim.keymap.set("n", "<M-j>", smartsplits.resize_down, { desc = "Resize down" })
 vim.keymap.set("n", "<M-h>", smartsplits.resize_left, { desc = "Resize left" })
 
-vim.keymap.set("n", "<C-w>k", smartsplits.swap_buf_up, { desc = "Swap window up" })
-vim.keymap.set("n", "<C-w>l", smartsplits.swap_buf_right, { desc = "Swap window right" })
-vim.keymap.set("n", "<C-w>j", smartsplits.swap_buf_down, { desc = "Swap window down" })
-vim.keymap.set("n", "<C-w>h", smartsplits.swap_buf_left, { desc = "Swap window left" })
+-- Override default movement
+vim.keymap.set("n", "<C-w>k", smartsplits.move_cursor_up, { desc = "Move up" })
+vim.keymap.set("n", "<C-w>l", smartsplits.move_cursor_right, { desc = "Move right" })
+vim.keymap.set("n", "<C-w>j", smartsplits.move_cursor_down, { desc = "Move down" })
+vim.keymap.set("n", "<C-w>h", smartsplits.move_cursor_left, { desc = "Move left" })
+
+-- Override default swapping
+vim.keymap.set("n", "<C-w>K", smartsplits.swap_buf_up, { desc = "Swap window up" })
+vim.keymap.set("n", "<C-w>L", smartsplits.swap_buf_right, { desc = "Swap window right" })
+vim.keymap.set("n", "<C-w>J", smartsplits.swap_buf_down, { desc = "Swap window down" })
+vim.keymap.set("n", "<C-w>H", smartsplits.swap_buf_left, { desc = "Swap window left" })

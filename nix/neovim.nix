a608c103b192dfdd912fcee316a49878437fbb2c
csvenke <csvenke@users.noreply.github.com>
2025-12-02 02:46:48 +0100

    refactor: move builtin plugin disabling to nix wrapper
    * extract plugin disable logic from lua config to nix writeText
    * load disable script via neovim --cmd flag for earlier execution
    * remove redundant plugin disable code from options.lua
    * consolidate all builtin plugin management in nix configuration

--- nix/neovim.nix
+++ nix/neovim.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  writeText,
   wrapNeovimUnstable,
   neovimUtils,
   neovim-unwrapped,
@@ -20,6 +21,23 @@ let
     buildInputs = config.dependencies;
   };
 
+  disableBuiltinPlugins = writeText "disable-builtin-plugins.lua" /* lua */ ''
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    vim.g.loaded_tarPlugin = 1
+    vim.g.loaded_zipPlugin = 1
+    vim.g.loaded_matchparen = 1
+    vim.g.loaded_tutor_mode_plugin = 1
+    vim.g.loaded_2html_plugin = 1
+    vim.g.loaded_remote_plugins = 1
+    vim.g.loaded_getscript = 1
+    vim.g.loaded_getscriptPlugin = 1
+    vim.g.loaded_vimball = 1
+    vim.g.loaded_vimballPlugin = 1
+    vim.g.loaded_logiPat = 1
+    vim.g.loaded_rrhelper = 1
+  '';
+
   neovimConfig =
     neovimUtils.makeNeovimConfig {
       withNodeJs = false;
@@ -34,6 +52,8 @@ let
         "PATH"
         ":"
         (lib.makeBinPath config.dependencies)
+        "--add-flags"
+        "--cmd 'luafile ${disableBuiltinPlugins}'"
       ];
     };
 in

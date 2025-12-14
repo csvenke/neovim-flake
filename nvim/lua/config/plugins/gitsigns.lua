098049be2853ece1cec6bfeb9f3b515d7a9cd2af
csvenke <csvenke@users.noreply.github.com>
2025-12-15 00:22:24 +0100

    refactor(gitsigns): update git blame keymaps
    * move toggle blame from <leader>gb to <leader>tg
    * add new <leader>gb for blame_line function

--- nvim/lua/config/plugins/gitsigns.lua
+++ nvim/lua/config/plugins/gitsigns.lua
@@ -10,7 +10,8 @@ gitsigns.setup({
       gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
     end
 
-    vim.keymap.set("n", "<leader>gb", gitsigns.toggle_current_line_blame, { desc = "toggle [g]it [b]lame" })
+    vim.keymap.set("n", "<leader>tg", gitsigns.toggle_current_line_blame, { desc = "[t]oggle [g]it blame" })
+    vim.keymap.set("n", "<leader>gb", gitsigns.blame_line, { desc = "[g]it [b]lame line" })
     vim.keymap.set("v", "gh", stage_visual_hunk, { desc = "git stage [h]unk" })
     vim.keymap.set("v", "gH", reset_visual_hunk, { desc = "git reset [H]unk" })
   end,

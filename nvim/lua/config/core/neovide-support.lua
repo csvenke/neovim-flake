c321d02a37e7bd93f7eb3366eec331226ad4a5e2
csvenke <csvenke@users.noreply.github.com>
2026-01-10 12:16:20 +0100

    fix: update neovide support config

--- nvim/lua/config/core/neovide-support.lua
+++ nvim/lua/config/core/neovide-support.lua
@@ -1,9 +1,11 @@
 if vim.g.neovide then
+  vim.g.neovide_scale_factor = 1.0
+
+  ---@param delta number
   local change_scale_factor = function(delta)
     vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
   end
 
-  vim.g.neovide_scale_factor = 1.0
   vim.g.neovide_position_animation_length = 0
   vim.g.neovide_cursor_animation_length = 0.00
   vim.g.neovide_cursor_trail_size = 0
@@ -12,19 +14,12 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_far_lines = 0
   vim.g.neovide_scroll_animation_length = 0.00
 
-  vim.keymap.set({ "n", "v" }, "<C-c>", '"+y', { desc = "copy to clipboard" })
-  vim.keymap.set({ "n", "v" }, "<C-v>", '"+P', { desc = "paste from clipboard" })
-  vim.keymap.set("i", "<C-v>", '<ESC>l"+Pli', { desc = "paste in insert mode" })
-  vim.keymap.set("c", "<C-v>", "<C-R>+", { desc = "paste in command mode" })
-
-  vim.keymap.set("n", "<C-=>", function()
+  vim.keymap.set("n", "<C-0>", function()
     change_scale_factor(1.25)
   end)
-
   vim.keymap.set("n", "<C-->", function()
     change_scale_factor(1 / 1.25)
   end)
-
   vim.keymap.set("n", "<C-+>", function()
     change_scale_factor(1 * 1.25)
   end)

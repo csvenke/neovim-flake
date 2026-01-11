2a468a269f79fed008c00ad0512d224bae65b5a8
csvenke <csvenke@users.noreply.github.com>
2026-01-11 12:51:41 +0100

    chore: update nordic-nvim plugin to latest commit
    * bump version from 6afe957 to 4f0245e
    * update sha256 hash for new revision

--- overlays/vim-plugins/nordic-nvim/package.nix
+++ overlays/vim-plugins/nordic-nvim/package.nix
@@ -2,12 +2,12 @@
 
 vimUtils.buildVimPlugin rec {
   pname = "nordic";
-  version = "6afe957722fb1b0ec7ca5fbea5a651bcca55f3e1";
+  version = "4f0245ed32a32436b2c6ae1a03f625a93a8c077a";
   src = fetchFromGitHub {
     owner = "AlexvZyl";
     repo = "nordic.nvim";
     rev = version;
-    sha256 = "sha256-NY4kjeq01sMTg1PZeVVa2Vle4KpLwWEv4y34cDQ6JMU=";
+    sha256 = "sha256-Mm+2VDpWc3abY4EUpg3f+kVjtEE/IZvRbPSxh9BjyfA=";
   };
   meta.homepage = "https://github.com/AlexvZyl/nordic.nvim";
 }

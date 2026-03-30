b6339c67cbbf13820ab3f994af37ee05e191b1c2
csvenke <csvenke@users.noreply.github.com>
2026-03-30 10:33:01 +0200

    refactor: remove deprecated neovimUtils.makeNeovimConfig

--- nix/neovim.nix
+++ nix/neovim.nix
@@ -1,54 +1,26 @@
 {
   lib,
-  writeText,
   wrapNeovimUnstable,
-  neovimUtils,
   neovim-unwrapped,
   src,
   plugins,
   runtimeDeps,
 }:
 
-let
-  disableBuiltinPlugins = writeText "disable-builtin-plugins.lua" /* lua */ ''
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-    vim.g.loaded_tarPlugin = 1
-    vim.g.loaded_zipPlugin = 1
-    vim.g.loaded_matchparen = 1
-    vim.g.loaded_tutor_mode_plugin = 1
-    vim.g.loaded_2html_plugin = 1
-    vim.g.loaded_remote_plugins = 1
-    vim.g.loaded_getscript = 1
-    vim.g.loaded_getscriptPlugin = 1
-    vim.g.loaded_vimball = 1
-    vim.g.loaded_vimballPlugin = 1
-    vim.g.loaded_logiPat = 1
-    vim.g.loaded_rrhelper = 1
+wrapNeovimUnstable neovim-unwrapped {
+  withNodeJs = false;
+  withPython3 = false;
+  withRuby = false;
+  wrapRc = true;
+  neovimRcContent = /* vim */ ''
+    set runtimepath^=${src}
+    set runtimepath+=${src}/after
   '';
-
-  neovimConfig =
-    neovimUtils.makeNeovimConfig {
-      withNodeJs = false;
-      withPython3 = false;
-      withRuby = false;
-      wrapRc = true;
-      customRC = /* vim */ ''
-        set runtimepath^=${src}
-        set runtimepath+=${src}/after
-      '';
-      inherit plugins;
-    }
-    // {
-      wrapperArgs = [
-        "--prefix"
-        "PATH"
-        ":"
-        (lib.makeBinPath runtimeDeps)
-        "--add-flags"
-        "--cmd 'luafile ${disableBuiltinPlugins}'"
-      ];
-    };
-in
-
-wrapNeovimUnstable neovim-unwrapped neovimConfig
+  inherit plugins;
+  wrapperArgs = [
+    "--prefix"
+    "PATH"
+    ":"
+    (lib.makeBinPath runtimeDeps)
+  ];
+}

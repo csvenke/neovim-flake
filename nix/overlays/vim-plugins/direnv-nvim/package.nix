dc563e65093f242073b5b83afdb2e7fe94e490a0
csvenke <csvenke@users.noreply.github.com>
2026-04-03 13:21:20 +0200

    chore(vim-plugins): update direnv-nvim to 5641462 and remove patch
    - Update version from 4dfc875 to 5641462
    - Update sha256 hash for new version
    - Remove disable-startup-notify.patch as it's no longer needed with the updated version

--- nix/overlays/vim-plugins/direnv-nvim/package.nix
+++ nix/overlays/vim-plugins/direnv-nvim/package.nix
@@ -2,15 +2,12 @@
 
 vimUtils.buildVimPlugin rec {
   pname = "direnv";
-  version = "4dfc8758a1deab45e37b7f3661e0fd3759d85788";
+  version = "564146278b3d5fe4ffa389cd103bab20f9b515d6";
   src = fetchFromGitHub {
     owner = "NotAShelf";
     repo = "direnv.nvim";
     rev = version;
-    sha256 = "sha256-KqO8uDbVy4sVVZ6mHikuO+SWCzWr97ZuFRC8npOPJIE=";
+    sha256 = "sha256-JbnGoZMApLtq4lSdGolcpKdsneolSOrzIi+O5yR2NDQ=";
   };
-  patches = [
-    ./patches/disable-startup-notify.patch
-  ];
   meta.homepage = "https://github.com/NotAShelf/direnv.nvim";
 }

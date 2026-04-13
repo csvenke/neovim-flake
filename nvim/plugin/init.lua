40e7f142a43c58b19380a725e4e835bc5aa96f2e
csvenke <csvenke@users.noreply.github.com>
2026-04-13 12:14:34 +0200

    refactor: update ui2 msg targets config

--- nvim/plugin/init.lua
+++ nvim/plugin/init.lua
@@ -1,5 +1,17 @@
 vim.loader.enable()
 
-require("vim._core.ui2").enable()
+require("vim._core.ui2").enable({
+  msg = {
+    targets = {
+      emsg = "msg",
+      echoerr = "msg",
+      lua_error = "msg",
+      rpc_error = "msg",
+    },
+    msg = {
+      timeout = 1200,
+    },
+  },
+})
 
 require("config")

ed8e19321fa0dba0bd0f0de674c86432e8da9578
csvenke <csvenke@users.noreply.github.com>
2026-04-14 15:37:36 +0200

    refactor: remove workspace plugin management system
    - Delete workspace.lua utility module
    - Remove workspace enter/exit calls from codediff plugin
    - Simplify codediff keymaps to direct command invocations
    - Remove codediff BufUnload autocmd hook
    - Simplify smart-splits keymaps by removing skip logic wrapper
    - Disable enableOnTabEnter in no-neck-pain configuration
    - Remove no-neck-pain workspace registration and skip checks

--- nvim/lua/config/plugins/codediff.lua
+++ nvim/lua/config/plugins/codediff.lua
@@ -1,5 +1,3 @@
-local workspace = require("config.lib.workspace")
-
 local explorer_width = 40
 
 require("codediff").setup({
@@ -45,36 +43,12 @@ require("codediff").setup({
   },
 })
 
-local function open_codediff(cmd)
-  workspace.enter()
-  vim.cmd(cmd)
-end
-
-vim.keymap.set("n", "<leader>gd", function()
-  open_codediff("CodeDiff")
-end, { desc = "[g]it [d]iff view" })
-
-vim.keymap.set("n", "<leader>gh", function()
-  open_codediff("CodeDiff history %")
-end, { desc = "[g]it [h]istory" })
-
-vim.keymap.set("n", "<leader>gH", function()
-  open_codediff("CodeDiff history")
-end, { desc = "[g]it [H]istory" })
-
-vim.keymap.set("n", "<leader>gr", function()
-  open_codediff("CodeDiff origin/HEAD")
-end, { desc = "[g]it code [r]eview" })
+vim.keymap.set("n", "<leader>gd", "<cmd>CodeDiff<cr>", { desc = "[g]it [d]iff view" })
+vim.keymap.set("n", "<leader>gh", "<cmd>CodeDiff history %<cr>", { desc = "[g]it [h]istory" })
+vim.keymap.set("n", "<leader>gH", "<cmd>CodeDiff history<cr>", { desc = "[g]it [H]istory" })
+vim.keymap.set("n", "<leader>gr", "<cmd>CodeDiff origin/HEAD<cr>", { desc = "[g]it code [r]eview" })
 
 local group = vim.api.nvim_create_augroup("user-codediff-hooks", { clear = true })
-
-vim.api.nvim_create_autocmd("BufUnload", {
-  group = group,
-  pattern = "codediff://*",
-  callback = function()
-    workspace.exit()
-  end,
-})
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
   group = group,
--- nvim/lua/config/plugins/no-neck-pain.lua
+++ nvim/lua/config/plugins/no-neck-pain.lua
@@ -1,11 +1,9 @@
-local workspace = require("config.lib.workspace")
-
 require("no-neck-pain").setup({
   width = 200,
   disableOnLastBuffer = true,
   autocmds = {
     enableOnVimEnter = true,
-    enableOnTabEnter = true,
+    enableOnTabEnter = false,
     skipEnteringNoNeckPainBuffer = true,
   },
   buffers = {
@@ -16,18 +14,3 @@ require("no-neck-pain").setup({
 })
 
 vim.keymap.set("n", "<leader>tn", "<cmd>NoNeckPain<cr>", { desc = "[t]oggle no [n]eck pain" })
-
-workspace.register("no-neck-pain", function()
-  local nnp = require("no-neck-pain")
-  return nnp.state and nnp.state.enabled
-end, function()
-  pcall(function()
-    vim.cmd("NoNeckPain")
-  end)
-end)
-
-workspace.register_skip("no-neck-pain", function(win_id)
-  local bufnr = vim.api.nvim_win_get_buf(win_id)
-  local ft = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
-  return ft == "no-neck-pain"
-end)

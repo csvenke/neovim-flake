d4df22f9673ac3272aa4c6ab93c5e44a204c11b5
csvenke <csvenke@users.noreply.github.com>
2026-05-05 13:34:21 +0200

    refactor(gitsigns): update gitsigns to require actions inside on attach

--- nvim/lua/config/plugins/gitsigns.lua
+++ nvim/lua/config/plugins/gitsigns.lua
@@ -1,17 +1,17 @@
-local gitsigns = require("gitsigns")
-
-gitsigns.setup({
+require("gitsigns").setup({
   on_attach = function()
+    local actions = require("gitsigns.actions")
+
     local function stage_visual_hunk()
-      gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      actions.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
     end
 
     local function reset_visual_hunk()
-      gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      actions.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
     end
 
-    vim.keymap.set("n", "<leader>tg", gitsigns.toggle_current_line_blame, { desc = "[t]oggle [g]it blame" })
-    vim.keymap.set("n", "<leader>gb", gitsigns.blame_line, { desc = "[g]it [b]lame line" })
+    vim.keymap.set("n", "<leader>tg", actions.toggle_current_line_blame, { desc = "[t]oggle [g]it blame" })
+    vim.keymap.set("n", "<leader>gb", actions.blame_line, { desc = "[g]it [b]lame line" })
     vim.keymap.set("v", "gh", stage_visual_hunk, { desc = "git stage [h]unk" })
     vim.keymap.set("v", "gH", reset_visual_hunk, { desc = "git reset [H]unk" })
   end,
